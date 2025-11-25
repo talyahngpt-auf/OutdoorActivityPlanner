045f909fdae08adec5afa4e1fe502724feccec61
NeldhydeQuizon <quizon.neldhyde@auf.edu.ph>
2025-11-25 18:07:43 +0800

    Update 3.1

---
+++ app/src/main/java/ph/edu/auf/thalia/hingpit/outdooractivityplanner/providers/LocationProvider.kt
@@ -0,0 +1,33 @@
+package ph.edu.auf.thalia.hingpit.outdooractivityplanner.providers
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
+
+class LocationProvider(context: Context) {
+
+    private val fusedLocationClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(context)
+
+    @SuppressLint("MissingPermission")
+    fun getLastLocation(onLocationReceived: (Location?) -> Unit) {
+        fusedLocationClient.getCurrentLocation(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            object : CancellationToken() {
+                override fun onCanceledRequested(p0: OnTokenCanceledListener) =
+                    CancellationTokenSource().token
+
+                override fun isCancellationRequested() = false
+            }
+        )
+            .addOnSuccessListener { location: Location? ->
+                onLocationReceived(location)
+            }
+    }
+}
